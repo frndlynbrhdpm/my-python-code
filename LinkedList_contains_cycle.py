12c302b243c06024522188b39bc47d40b785d3b3
shwetabhagat14 <40577608+shwetabhagat14@users.noreply.github.com>
2018-07-17 11:53:44 -0700

    Completed the logic for "contains cycle"

--- LinkedList_contains_cycle.py
+++ LinkedList_contains_cycle.py
@@ -2,5 +2,29 @@ class node:
 	def __init__(self, val):
 		self.val=val
 		self.next=None
+	
+class Solution:
+	def contains_cycle(self, head):
+		curr=head
+		
+		while curr.next!=None:
+			curr=curr.next
+			if curr.next==head:
+				return True
+		return False
+	
+#Driver code
+a=node(1)
+b=node(2)
+c=node(3)
+d=node(4)
+e=node(5)
 
+a.next=b
+b.next=c
+c.next=d
+d.next=e
+e.next=a
 
+s=Solution()
+print(s.contains_cycle(a))
